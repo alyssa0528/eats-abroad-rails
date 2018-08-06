63aa48ae32bc46b0696e6352d21f6b01bc872119
alyssa0528 <alyssa.kim@gmail.com>
2018-08-06 08:55:51 +0100

    add new and create actions and view file for chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,25 @@
-class ChefsController < ApplicationController 
+class ChefsController < ApplicationController
+
+  #action for signup form
+  def new
+    @chef = Chef.new
+  end
+
+  #post action for signup
+  def create
+    @chef = Chef.new(chef_params)
+
+    if @chef
+      redirect_to chef_path(@chef)
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name, :email, :password_digest, :uid, :hometown, :employer)
+  end
 end
