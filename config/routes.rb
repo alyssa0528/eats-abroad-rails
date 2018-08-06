1e51d76e84deb03674dd64b6ceea5baa95810e29
alyssa0528 <alyssa.kim@gmail.com>
2018-08-06 08:18:22 +0100

    add signin route and chefs resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#home'
+  get '/signin' => 'sessions#new'
   resources :cities, only: [:index, :show]
   resources :restaurants, only: [:index, :show, :new, :create]
+  resources :chefs, only: [:index, :new, :create, :show]
 end
