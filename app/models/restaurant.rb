8e623f78a76660b906db7b230a1e61b262dd803d
alyssa0528 <alyssa.kim@gmail.com>
2018-08-08 12:20:51 +0100

    start coding comment= methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,5 +5,21 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :city_id, presence: true
-  
+
+  def comment=(comments)
+    comments.each do |content|
+      if content != ""
+        self.comments.build(content: content)
+      end
+    end
+  end
+
+  def comment
+    if self.comments
+      self.comments.collect do |comment|
+        comment.content
+      end
+    end 
+  end
+
 end
