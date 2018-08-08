21a961b98cce91edbbea78868af07e2a85f6fdd0
alyssa0528 <alyssa.kim@gmail.com>
2018-08-08 10:54:57 +0100

    start coding create action in restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
       @restaurants = City.find(params[:city_id]).restaurants
     elsif params[:chef_id]
       @restaurants = Chef.find(params[:chef_id]).restaurants
+      @chef = Chef.find(params[:chef_id])
     else
       @restaurants = Restaurant.all
     end
@@ -16,7 +17,27 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  #GET /restaurants/new
   def new
-    @restaurant = Restaurant.new 
+    @restaurant = Restaurant.new
   end
+
+  #POST
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      @restaurant.save
+      redirect_to chef_restaurant(@restaurant)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :cuisine, :city_id)
+  end
+
 end
