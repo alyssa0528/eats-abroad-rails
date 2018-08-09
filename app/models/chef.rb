193c0903fa500fe58901115b4504ac21f17ba6d2
alyssa0528 <alyssa.kim@gmail.com>
2018-08-09 12:04:03 +0100

    add unrecommended_restaurants method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,8 @@ class Chef < ApplicationRecord
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
   has_secure_password
+
+  def unrecommended_restaurants
+    (Restaurant.all.map{|r| r.id} - self.restaurant_ids).map{|id| Restaurant.find(id)}
+  end
 end
