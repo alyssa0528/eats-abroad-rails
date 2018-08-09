fe56d5555e34627c29ed30f722b5c3812b92d2ae
alyssa0528 <alyssa.kim@gmail.com>
2018-08-09 14:50:43 +0100

    update create action to account for duplicate restaurant entry

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -28,18 +28,17 @@ class RestaurantsController < ApplicationController
   def new
     @chef = current_user
     @restaurant = Restaurant.new
-    @restaurant.comments.build
+    #@restaurant.comments.build
   end
 
   #POST
   def create
-    #for creating brand new restaurant
-    #binding.pry
-    #@new_restaurant = current_user.restaurants.build(restaurant_params)#(:name, :cuisine, :city_id))
-    binding.pry
+    @chef = current_user
+    @restaurant = Restaurant.new
+
     #to handle existing restaurant addition
-    if params[:restaurant][:id]
-      @restaurant = Restaurant.find(params[:restaurant][:id])
+    if params[:restaurant][:id] #will have id from dropdown
+      @restaurant = Restaurant.find(params[:restaurant][:id]) #find the restaurant instance
       #@restaurant.chefs << current_user #build the association
       #current_user.restaurants << @restaurant
       #new_restaurant = current_user.restaurants.build(restaurant_params)
@@ -47,15 +46,26 @@ class RestaurantsController < ApplicationController
     #@new_restaurant.save
     #@new_restaurant.comments.build(params[:restaurant][:comment_contents => []])
     #@new_restaurant.save
-      redirect_to new_restaurant_comment_path(@restaurant)
+      redirect_to new_restaurant_comment_path(@restaurant) #go to that restaurant's comments page
 
-    #to handle new restaurant addition - WORKING
-    elsif params[:restaurant][:name]
-      new_restaurant = current_user.restaurants.build(restaurant_params)
-      new_restaurant.save
-      redirect_to new_restaurant_comment_path(new_restaurant)
+    #to handle brand new restaurant addition
+    elsif params[:restaurant][:name] != "" && params[:restaurant][:city_id] != ""
+      #if name and city ID are provided (required per validations)
+      if Restaurant.find_by(:name => params[:restaurant][:name]) # if name matches existing an restaurant
+        @restaurant = Restaurant.find_by(:name => params[:restaurant][:name]) #find the instance, assign to @restaurant
+        redirect_to new_restaurant_comment_path(@restaurant) #go to new comment page
+      else  #if it's a brand new restaurant not in the database
+        @new_restaurant = current_user.restaurants.build(restaurant_params)
+        if @new_restaurant.save
+          @new_restaurant.save
+          redirect_to new_restaurant_comment_path(@new_restaurant)
+        else
+          render :new
+        end
+      end
     else
       render :new
+      @chef = current_user
     end
     #if Restaurant.find(params[:restaurant][:id]) #if the restaurant was selected from drop-down
     #  @restaurant = Restaurant.find(params[:restaurant][:id])
