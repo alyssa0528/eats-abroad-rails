8d4dc2ee71a0d88bef8c697024f683adae43d4ee
alyssa0528 <alyssa.kim@gmail.com>
2018-09-29 14:02:26 +0100

    add JS code for restaurant show page from index page

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -13,12 +13,27 @@ const bindClickListeners = function() {
         restaurants.forEach(function(restaurant) {
           let newRestaurant = new Restaurant(restaurant)
           let restaurantHtml = newRestaurant.formatIndex()
-          //console.log(restaurantHtml)
           $('#body-container').append(restaurantHtml)
-          //console.log(newRestaurant)
         })
       })
-  })
+    })
+    //for SHOW restaurant
+  $(document).on('click', '.show-link', function(e) {
+    e.preventDefault();
+    $('#body-container').html('')
+    let id = $(this).attr("data-id")
+    history.pushState(null, null, `restaurants/${id}`)
+    fetch(`/restaurants/${id}.json`)
+      .then(response => response.json())
+      .then(restaurant => {
+        // append restaurant JSON object to the #body-container div
+        let newRestaurant = new Restaurant(restaurant);
+        let restaurantHtml = newRestaurant.formatShow();
+        console.log(restaurantHtml)
+        $('#body-container').append(restaurantHtml)
+      })
+    })
+  }
 
 
 function Restaurant(restaurant) {
@@ -35,5 +50,14 @@ Restaurant.prototype.formatIndex = function() {
     <a href="/restaurants/${this.id}" class="show-link" data-id="${this.id}"><h1>${this.name}</h1></a>
     <p>${this.cuisine} | ${this.city.name}</p>
   `
+  return restaurantHtml
+}
+
+Restaurant.prototype.formatShow = function() {
+  let restaurantHtml = `
+    <h1>${this.name}</h1>
+    <p>Cuisine: ${this.cuisine}</p>
+    <p>City: ${this.city.name}</p>
+  ` //create button for "More Info" that will show comments for the restaurant
   return restaurantHtml
 }
