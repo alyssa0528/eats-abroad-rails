1b1fee9cc6e4519d86ae557e0927706ade43c011
alyssa0528 <alyssa.kim@gmail.com>
2019-01-04 10:41:19 +0000

    remove binding.pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   def home
-    #binding.pry
     @chef = Chef.find_by(:email => session[:email])
     if !@chef
       render :layout => false
-    end 
+    end
   end
 end
